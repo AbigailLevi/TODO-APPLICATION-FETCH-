06d2fad120bd955f4e4c970abf258d260a2cdb7f
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-18 15:28:36 -0500

    yolo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+const url = 'https://assets.breatheco.de/apis/fake/todos/user/AbigailLevi';
 
 const Todos = () => {
     const [todos, setTodos] = useState([])
     const [inputValue, setInputValue] = useState('');
-    const deleteTodos = indexToDelete => {
 
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(res => {
+                console.log('response:' + JSON.stringify(res))
+                if (res.msg) {
+                    fetch(url, {
+                        method: 'POST',
+                        body: JSON.stringify([{}]),
+                        headers: { 'Content-Type': 'application/json' }
+                    })
+                        .then(res => res.json())
+                        .then(res => console.log(res))
+                        .catch(err => console.log('error: ${err}'));
+                } else {
+                    console.log('user exits, here is response:' + JSON.stringify(res));
+                }
+            })
+            .catch(err => console.log(`error: ${err}`));
+    }, [todos]);
+
+    const deleteTodos = indexToDelete => {
         setTodos(prevTodos => {
             return prevTodos.filter((value, index) => {
                 return indexToDelete !== index;
@@ -12,39 +34,6 @@ const Todos = () => {
         })
     }
 
-        /*fetch("https://jsonplaceholder.typicode.com/Abigaillevi")
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    console.log("Bad response");
-                }
-            })
-            .then(data => {
-                setTodos({ todos: data });
-                console.log(data);
-            })
-            .catch(error => console.log(error));*/
-    
-    
-        /*fetch("https://jsonplaceholder.typicode.com/Abigaillevi", {
-            method: "PUT", // or 'POST'
-            body: JSON.stringify(todos), // data can be `string` or {object}!
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(res => res.json())
-            .then(response => {
-                if (!response.msg) {
-                    todos;
-                } else {
-                    console.log(response.msg);
-                }
-                console.log(response);
-            })
-            .catch(error => console.error("Error:", error));*/
-    
 
 
     return (
